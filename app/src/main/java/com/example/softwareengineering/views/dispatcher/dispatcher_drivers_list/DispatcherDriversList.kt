b03a65b914a00cbff4279d7b6584beb9aae1050b
harkor15 <kibkib1998@gmail.com>
2021-07-04 18:15:19 +0200

    Driver and dispachers fragments

--- app/src/main/java/com/example/softwareengineering/views/dispatcher/dispatcher_drivers_list/DispatcherDriversList.kt
+++ app/src/main/java/com/example/softwareengineering/views/dispatcher/dispatcher_drivers_list/DispatcherDriversList.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.navigation.Navigation
 import com.example.softwareengineering.R
 
 class DispatcherDriversList : Fragment() {
@@ -15,6 +16,14 @@ class DispatcherDriversList : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         view.findViewById<Button>(R.id.ddl_button_back).setOnClickListener {
             activity?.onBackPressed()
+        }
+        val navController = Navigation.findNavController(view)
+
+        view.findViewById<Button>(R.id.ddl_driver_info).setOnClickListener {
+            navController.navigate(R.id.action_dispatcherDriversList_to_dispatcherDriverDetails2)
+        }
+        view.findViewById<Button>(R.id.ddl_vehicle_info).setOnClickListener {
+            navController.navigate(R.id.action_dispatcherDriversList_to_dispatcherVehicleDetails)
         }
     }
 
